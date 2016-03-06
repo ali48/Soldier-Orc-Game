342184a050e12a0c33fd47661c6442fa225b843d
Muhammad Ali <muhammadali@muhammads-macbook-air.local>
2016-03-06 12:08:03 -0500

    Initial Commit

---
+++ Soldier_Orc-Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soldier_Orc-Game
+//
+//  Created by Muhammad Ali on 2016-03-06.
+//  Copyright © 2016 Muhammad Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
